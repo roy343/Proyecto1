d48fe4ee8c43977e352d07f5427aef1f17354894
Roy <roychav99@gmail.com>
2024-04-04 01:19:59 -0600

    Lector de inputs analogicos

--- sketch_apr2a/sketch_apr2a.ino
+++ sketch_apr2a/sketch_apr2a.ino
@@ -1,9 +1,10 @@
 const int A = 2;
 const int B = 3;
 const int C = 4;
+const int TRIG = 5;
+const int ECHO = 6;
+const int test = 7;
 
-#define TRIG 5
-#define ECHO 6
 
 void setup() {
   Serial.begin(9600); 
@@ -12,6 +13,8 @@ void setup() {
   pinMode(C, OUTPUT);
   pinMode(TRIG, OUTPUT);
   pinMode(ECHO, INPUT);
+  pinMode(test, OUTPUT);
+
 }
 
 void loop() {
@@ -20,23 +23,35 @@ void loop() {
   int a;
   int b;
   int c;
-  
+  digitalWrite(test, LOW);
   digitalWrite(TRIG, LOW);
   delayMicroseconds(2);
   digitalWrite(TRIG, HIGH);
   delayMicroseconds(5);
   digitalWrite(TRIG, LOW);
 
+  int inA = analogRead(A0);
+  int inB = analogRead(A1);
+  int inC = analogRead(A2);
+
+  int boolA = intToBool(inA);
+  int boolB = intToBool(inB);
+  int boolC = intToBool(inC);
+
+  Serial.println(boolA);
+  Serial.println(boolB);
+  Serial.println(boolC);
+
   int time = pulseIn(ECHO, HIGH);
   int cm = msToCm(time);
   int range = rangeLimiter(cm);
-  Serial.println("Decimal");
-  Serial.println(range);
-  Serial.println("Hexadecimal");
-  Serial.println(range, HEX);
-  Serial.println("Gray");
+  //Serial.println("Decimal");
+ // Serial.println(range);
+ // Serial.println("Hexadecimal");
+ // Serial.println(range, HEX);
+  //Serial.println("Gray");
   y = grayCode(range);
-  Serial.println(y);
+  //Serial.println(y);
   delay(1000);
   
   a = y/100%10;
@@ -47,7 +62,7 @@ void loop() {
   digitalWrite(B, b);
   digitalWrite(C, c);
 
-  delay(100);
+  delay(1000);
 }
 
 int grayCode(int num){
@@ -61,7 +76,16 @@ int msToCm(int ms){
 }
 
 int rangeLimiter(int x){
-    if(x >= 3 & x <=10){
+    if(x >= 3 && x <=10){
     return x-3;
   }
+}
+
+int intToBool(int num){
+  if(num == 0){
+    return 0;
+  }else{
+    return 1;
+  }
+
 }
